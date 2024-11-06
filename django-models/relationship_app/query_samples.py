c9b4f57e17006c600821ea0b96934a8f902f2fe6
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-06 02:16:06 -0800

    Master Django’s ORM capabilities by creating a set of models that demonstrate the use of ForeignKey, ManyToMany, and OneToOne relationships.

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -13,5 +13,7 @@ def get_books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
+    library = Library.objects.get(library=library_name)
     return library.librarian
+
+
