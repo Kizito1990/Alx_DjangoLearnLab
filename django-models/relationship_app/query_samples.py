2aade0794b777928449d39b30d5891dbb72f17f3
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-06 02:07:38 -0800

    modeified quer_samples.py file

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 author = Author.objects.get(name=author_name)
 
 # List all books in a library
-library = Library.Book.objects.all()
+"Library.objects.get(name=library_name)", "books.all()"
 
 # Retrieve the librarian for a library
 library = Library.Librarian.objects.all()
