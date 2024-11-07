4b1d25a665e6caafedbf02c38d4c6feba3671df4
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-07 04:50:52 -0800

    views that display information about books and libraries. Implement both function-based and class-based views to handle these displays and configure the URL patterns to route these views correctly.

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,17 +6,17 @@ from django.views.generic import DetailView
 def list_books(request):
     books = Book.objects.all()
     context = {"books":books}
-    return render(request, "book/list_books.html", context)
+    return render(request, "relationship_app/list_books.html", context)
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'book/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
 
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(self, **kwargs)
-        context['library_list'] = Library.books.all()
+        context['library'] = Library.books.all()
         return context
 
     
