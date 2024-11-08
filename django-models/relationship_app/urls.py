77343206d05ed0b48818bbfcea4e70e168c6e2ed
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-08 05:54:08 -0800

    Added authication features

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import list_books, LoginView, LogoutView, register
 urlpatterns = [
     path('', views.list_books, name='list_books'),  
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name = 'login.html"), name='login'),
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="logout.hyml"), name='logout'),
     path('register/', views.register, name='register'),
 
