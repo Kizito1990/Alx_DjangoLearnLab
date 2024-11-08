a866683492bdddc98554d7178cd98506350933f1
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-08 08:13:28 -0800

    Added a user profile

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="logout.hyml"), name='logout'),
     path('register/', views.register, name='register'),
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
 
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 
 
 # Create your views here.
@@ -42,4 +43,24 @@ def register(request):
             return redirect('home')  # Adjust 'home' to your app's homepage or desired redirection.
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})    
+    return render(request, 'register.html', {'form': form})
+
+
+
+
+def check_role(role):
+    def decorator(user):
+        return user.is_authenticated and user.userprofile.role == role
+    return decorator
+
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
