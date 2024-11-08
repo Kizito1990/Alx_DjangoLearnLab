33a56f75e8c2ef39896043ebe3e82e5c037694ba
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-08 08:45:52 -0800

    Added a user profile

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,11 +5,18 @@ from .views import list_books, LoginView, LogoutView, register
 urlpatterns = [
     path('', views.list_books, name='list_books'),  
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="logout.hyml"), name='logout'),
     path('register/', views.register, name='register'),
+
     path('admin_view/', views.admin_view, name='admin_view'),
     path('librarian_view/', views.librarian_view, name='librarian_view'),
     path('member_view/', views.member_view, name='member_view'),
+
+
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 
 ]
