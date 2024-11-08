110e9c93ca890111176a2b3245e91c3e9c0d3c12
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-08 05:40:19 -0800

    Implement views and templates for user login, logout, and registration to demonstrate how Django manages user sessions and permissions.

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Library
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -20,4 +24,22 @@ class LibraryDetailView(DetailView):
         context['library'] = Library.books.all()
         return context
 
-    
+# Login view
+class LoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+# Logout view
+class LogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Adjust 'home' to your app's homepage or desired redirection.
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})    
