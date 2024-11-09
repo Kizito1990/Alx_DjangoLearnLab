9fd8db55d9a055d1cfdb67bbb4547428bce7b0e5
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-09 01:27:12 -0800

    Added a templates folder

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -45,7 +45,7 @@ def register(request):
             return redirect('home')  # Adjust 'home' to your app's homepage or desired redirection.
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'relationship_app/register.html', {'form': form})
 
 
 
