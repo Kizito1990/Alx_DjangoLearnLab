b11314bcdc3da99dc2e6294b471fa428152d9cae
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-09 01:47:31 -0800

    Added a templates folder

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required,user_passes_test
 from .forms import BookForm
+from .models import UserProfile
 
 # Create your views here.
 def list_books(request):
@@ -50,25 +51,30 @@ def register(request):
 
 
 
-def check_role(role):
-    def decorator(user):
-        return user.is_authenticated and user.userprofile.role == role
-    return decorator
+# Helper functions for role checking
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-@user_passes_test(check_role('Admin'))
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(check_role('Librarian'))
+# Librarian view
+@user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-@user_passes_test(check_role('Member'))
+# Member view
+@user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
-
-
-
 
 
 
