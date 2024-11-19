3bfbc29be2cacb14768ba96b2c10549dd8f0581e
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-19 07:33:34 -0800

    This will introduce you to the core components of DRF, including serializers and views.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .serializers import BookSerializer
-from rest_framework import generic
+from rest_framework import generics.ListAPIView
 
 # Create your views here.
 
