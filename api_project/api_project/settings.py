0b028eb49e182580efc0cfae2ee65693d223231f
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-21 07:55:28 -0800

    Secure your API endpoints by implementing various authentication schemes and permission settings in Django REST Framework.

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.permissions.IsAuthenticated',
+    'rest_framework.authtoken',
     'api',
 ]
 
