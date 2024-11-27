2a41f82b817370b68e8584082689984e43a59b23
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-11-27 04:06:33 -0800

    creating custom serializers that handle complex data structures and nested relationships.

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        self.Author
+
+class Book(models.Model):
+    title = models.CharField(max_length= 50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.DateField()
+
+    def __str__(self) -> str:
+        return self.title
---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from.models import Author, Book
+from django.utils import timezone
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'birth_date']
+
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author_name = serializers.CharField(source = 'author.name', read_only=True)
+
+    class Meta:
+        model = Book
+        fields =['id', 'title', 'author', 'author_name', 'published_date', 'isbn']
+
+
+    def validate_published_date(self, value):
+        if value > timezone.now().date():
+            raise serializers.ValidationError("The published date cannot be in the future.")
+            return value
+~                                                                                                                                                    
+~                        
