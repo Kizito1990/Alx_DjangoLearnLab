e1d65dd1b8854d8384a2ae7695cb0a6538dfddad
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-12-17 01:28:04 -0800

    Set up a new Django project specifically for a blog, which includes configuring the environment, establishing the initial models, and preparing the base templates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title =  models.CharField(max_length=200)
     content = models.TextField()
-    published_dat = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
