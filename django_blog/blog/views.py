5a4abd85c6da1cb3bf897c52bc4780ffb59fde27
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-12-17 06:37:27 -0800

    Added a blog crude operation

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,37 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from .forms import CustomUserCreationForm
-from django.contrib.auth.forms import UserChangeForm
-
-# Registration View
-class RegisterView(CreateView):
-    form_class = CustomUserCreationForm
-    template_name = "registration/register.html"
-    success_url = reverse_lazy("login")
-
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        return redirect("profile")
-
-# Profile View
-@login_required
-def profile_view(request):
-    if request.method == "POST":
-        user_form = UserChangeForm(request.POST, instance=request.user)
-        if user_form.is_valid():
-            user_form.save()
-            return redirect("profile")
-    else:
-        user_form = UserChangeForm(instance=request.user)
-    return render(request, "profile.html", {"user_form": user_form})
-
-
-
-
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
