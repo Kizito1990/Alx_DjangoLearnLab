967b892536f3b4f60364b92138bb7b07c4c993f6
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-12-17 06:40:20 -0800

    Added a blog crude operation

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,12 +7,6 @@ from .views import RegisterView, profile_view, PostListView,
     PostDeleteView
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="registration/logged_out.html"), name="logout"),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("profile/", profile_view, name="profile"),
-
-
     path('', PostListView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
