e9e9fea2e4c275bdf2b27fee70fd432101ba1ade
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-12-19 00:34:03 -0800

    add advanced query capabilities to your existing views that manage the Book model. This will allow API consumers to filter, search, and order the books based on different criteria.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters 
 from rest_framework import status
-
+from django_filters import rest_framework
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
